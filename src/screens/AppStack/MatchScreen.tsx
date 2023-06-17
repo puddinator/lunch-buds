2212962978e40bebb912531d6476de3747189a1e
James <42086157+puddinator@users.noreply.github.com>
2023-06-17 17:06:27 +0800

    demonstration next button

--- src/screens/AppStack/MatchScreen.tsx
+++ src/screens/AppStack/MatchScreen.tsx
@@ -1,40 +1,50 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { Icon, Input, Pressable, Text, Image, Stack } from "native-base";
 import { useState } from "react";
-import { Dimensions, View } from "react-native";
+import { Dimensions, TouchableOpacity, View } from "react-native";
 
 const handlePress = () => {};
 
 export const MatchScreen = () => {
-  const WINDOW_HEIGHT = Dimensions.get("window").width;
+  // const WINDOW_HEIGHT = Dimensions.get("window").width;
+  // const [searchTerm, setSearchTerm] = useState("");
 
-  const [searchTerm, setSearchTerm] = useState("");
+  const next = require("../../../assets/images/next-button.png");
 
   return (
-    <>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Stack space={10}>
-          <Text fontSize={"3xl"} color="white" fontWeight={600}>
-            Let's make your next travel incredible
-          </Text>
-          <Input
-            size="xl"
-            InputLeftElement={
-              <Pressable onPress={() => handlePress()}>
-                <Icon
-                  as={<MaterialIcons name="search" />}
-                  size={5}
-                  mr="2"
-                  color="grey"
-                />
-              </Pressable>
-            }
-            placeholder="Search for guides, users, attractions..."
-            placeholderTextColor="black"
-            onChangeText={setSearchTerm}
-          />
-        </Stack>
-      </View>
-    </>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Find your LunchBuds!</Text>
+      <TouchableOpacity onPress={() => handlePress()} style={{ flex: 1 }}>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <Text>Search</Text>
+          <Image source={next} alt="itinero logo" size="xs" />
+        </View>
+      </TouchableOpacity>
+    </View>
+    // <>
+    //   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    //     <Stack space={10}>
+    //       <Text fontSize={"3xl"} color="white" fontWeight={600}>
+    //         Let's make your next travel incredible
+    //       </Text>
+    //       <Input
+    //         size="xl"
+    //         InputLeftElement={
+    //           <Pressable onPress={() => handlePress()}>
+    //             <Icon
+    //               as={<MaterialIcons name="search" />}
+    //               size={5}
+    //               mr="2"
+    //               color="grey"
+    //             />
+    //           </Pressable>
+    //         }
+    //         placeholder="Search for guides, users, attractions..."
+    //         placeholderTextColor="black"
+    //         onChangeText={setSearchTerm}
+    //       />
+    //     </Stack>
+    //   </View>
+    // </>
   );
 };
