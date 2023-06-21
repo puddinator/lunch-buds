555cc4122d05f6c51fff39fda7244c07f9bb11a2
James <42086157+puddinator@users.noreply.github.com>
2023-06-21 12:52:34 +0800

    modify profiledata

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,33 +1,7 @@
+import { useNavigation } from "@react-navigation/native";
 import { HStack, Image, Text, VStack } from "native-base";
 import { TouchableOpacity } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 import { NewMatchesStackNavigationProps } from "../routes/NewMatchesRoutes";
-import { useState } from "react";
-
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    name: "Tommy Tan",
-    match: "Data Science",
-    otherInterests: "Bouldering, Baking",
-    number: "91234567",
-  },
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb",
-    name: "Joelle Tan",
-    match: "Data Science",
-    otherInterests: "Rock music, Swimming",
-    number: "92345678",
-  },
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-",
-    name: "Bryan Tan",
-    match: "Data Science",
-    otherInterests: "Reading, Skiing",
-    number: "93456789",
-  },
-];
-
 interface IProps {
   name: string;
   match: string;
