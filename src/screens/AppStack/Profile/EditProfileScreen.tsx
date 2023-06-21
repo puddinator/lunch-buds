82b7a14988834deebf201c19d951aad0e65cafe2
James <42086157+puddinator@users.noreply.github.com>
2023-06-21 11:05:48 +0800

    add edit profile screen

--- src/screens/AppStack/Profile/EditProfileScreen.tsx
+++ src/screens/AppStack/Profile/EditProfileScreen.tsx
@@ -1,117 +1,72 @@
-import { Text, Image, Button } from "native-base";
+import { Text, Image, Button, VStack, HStack, Input } from "native-base";
 import { useContext } from "react";
-import { View, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import { AuthContext } from "../../../contexts/AuthContext";
 
 export const EditProfileScreen = () => {
   const { signOut } = useContext(AuthContext);
 
-  return (
-    // <View style={styles.background}>
-    <SafeAreaView style={styles.safeAreaContainer}>
-      <View style={styles.editButton}>
-        <TouchableOpacity onPress={signOut}>
-          <Text fontSize={20}>Edit</Text>
-        </TouchableOpacity>
-      </View>
+  const windowWidth = Dimensions.get("window").width;
 
-      <View style={styles.profileContainer}>
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "#FFFBEC",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <VStack flex={1} space={5} justifyContent={"center"}>
         <Image
           source={require("../../../../assets/images/ProfileIcon.png")}
-          style={styles.profileIcon}
+          style={{
+            width: 160,
+            height: 160,
+            alignSelf: "center",
+          }}
         />
 
-        <View style={styles.row}>
-          <Text style={styles.profileLabel}> Name:</Text>
-          <Text style={styles.profileValue}> Tan Xiao Ming </Text>
-        </View>
+        <VStack space={3} width={windowWidth * 0.8}>
+          <HStack alignItems={"center"}>
+            <Text flex={3} style={{ fontSize: 20, textAlign: "right" }}>
+              Name: &nbsp;
+            </Text>
+            <Input flex={4} style={{ fontSize: 20 }}>
+              Tan Xiao Ming
+            </Input>
+          </HStack>
+          <HStack alignItems={"center"}>
+            <Text flex={3} style={{ fontSize: 20, textAlign: "right" }}>
+              H/P: &nbsp;
+            </Text>
+            <Input flex={4} style={{ fontSize: 20 }}>
+              912345678
+            </Input>
+          </HStack>
+          <HStack>
+            <Text flex={3} style={{ fontSize: 20, textAlign: "right" }}>
+              Interests: &nbsp;
+            </Text>
+            <VStack flex={4} space={2}>
+              <Input style={{ fontSize: 20 }}>Example 1</Input>
+              <Input style={{ fontSize: 20 }}>Example 2</Input>
+            </VStack>
+          </HStack>
+        </VStack>
+      </VStack>
 
-        <View style={styles.row}>
-          <Text style={styles.profileLabel}> H/P:</Text>
-          <Text style={styles.profileValue}> 912345678 </Text>
-        </View>
-
-        <View style={styles.row}>
-          <Text style={styles.profileLabel}> Interest:</Text>
-          <Text style={styles.interests}>insert buttons here</Text>
-        </View>
-      </View>
-
-      <View style={{ flex: 1, alignItems: "center", marginBottom: 50 }}>
-        <Button style={{ width: "40%" }} onPress={signOut}>
-          <Text>Sign out</Text>
+      <View style={{ marginBottom: 30 }}>
+        <Button onPress={() => {}}>
+          <Text style={{ fontSize: 20 }}>Update</Text>
         </Button>
       </View>
     </SafeAreaView>
-    // </View>
   );
 };
-
-const styles = StyleSheet.create({
-  //  background: {
-  // backgroundColor: "black",
-  // },
-  safeAreaContainer: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#FFFBEC",
-  },
-  editButton: {
-    position: "relative",
-    top: 0,
-    left: 0,
-    margin: 20,
-    alignSelf: "flex-start",
-    justifyContent: "center",
-    marginLeft: 30,
-    backgroundColor: "#D9D9D9",
-    padding: 10,
-    borderRadius: 10,
-  },
-  // profileIconContainer: {
-  // backgroundColor: "#D9D9D9",
-  // },
-  profileIcon: {
-    width: 120,
-    height: 120,
-    alignSelf: "center",
-    justifyContent: "center",
-    marginBottom: 60,
-  },
-  profileContainer: {
-    flex: 1,
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "flex-start", // Align items to the left side
-    paddingLeft: 0,
-    paddingBottom: 40, // Add bottom padding to shift text upwards
-  },
-  row: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginBottom: 10,
-    width: "80%",
-  },
-  ageRow: {
-    marginBottom: 30, //gap between hp row and interests row
-  },
-  profileLabel: {
-    textAlign: "right",
-    width: "50%",
-    paddingRight: 10,
-    fontSize: 20,
-  },
-  profileValue: {
-    textAlign: "left",
-    width: "50%",
-    paddingLeft: 10,
-    fontSize: 20,
-  },
-  interests: {
-    textAlign: "left",
-    width: "50%",
-    paddingLeft: 10,
-    fontSize: 10,
-    marginBottom: 180,
-  },
-});
